083f9336368b937bff9c39376be72ae5aa3900f9
imran <imran@yandex.ru>
2024-08-21 17:56:27 +0300

    add_files

---
+++ scrip.js
@@ -0,0 +1,26 @@
+let j=20;
+j=40;
+console.log (j)
+let a=2;
+let b=2;
+let c=2;
+let d=a+b*c;
+console.log (d)//
+
+let message="Goodbye World";
+console.log (message)
+
+var name;
+var address;
+var phone;
+    if(confirm("Do you want to register?")) {
+         name = prompt("What is your name?");
+         alert("Hi, " + name)
+         address = prompt("Where are you from?" + name);
+         phone = prompt("enter your number!" + name);
+    } else
+    alert ("go away!!!:(");
+
+console.log ("Студент - " + name)
+console.log ("Адрес - " + address)
+console.log ("Номер телефона - " + phone)
